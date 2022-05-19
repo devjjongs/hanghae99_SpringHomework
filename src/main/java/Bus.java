cf7c1f44c9d46cff9ba28ce3321edd818934e010
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-20 00:48:54 +0900

    코드 추가 및 수정
    Bus.java
    - 버스 탑승 메소드 추가
    
    Transport.java
    - 변수 자료형 수정

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,26 +1,43 @@
-public class Bus extends Transport implements StartDriving,Boarding,ChangeSpd,ChangeStatus{
+public class Bus extends Transport implements StartDriving, Boarding, ChangeSpd, ChangeStatus {
+    int newPassenger;
+    int noBoarding;
+    int boardingPermit;
 
-    public Bus(int transNum, int fuel, int nowSpeed, int maxPassenger, int nowPassenger, int fare, String status) {
-        super(transNum, fuel, 0, 45, 1, 100, status);
+    public Bus(int transNum, int fuel, int nowSpeed, int maxPassenger, int nowPassenger, int fare, Status status) {
+        super(201, 100, 0, 30, 1, 1000, status.운행중);
     }
 
     @Override
-    public void board(int nowSpeed, int maxPassenger, int nowPassenger, String status) {
+    public void board(int nowSpeed, int maxPassenger, int nowPassenger, Status status) {
+        if (nowSpeed == 0) {
+            if ((nowPassenger + newPassenger) <= 30) {
+                System.out.println("승객 " + newPassenger + "명 탑승");
+            } else if ((nowPassenger + newPassenger) > 30) {
+                boardingPermit = 30 - nowPassenger;
+                noBoarding = newPassenger - boardingPermit;
+                System.out.println("승객 " + boardingPermit + " 명 탑승, " + noBoarding + "명 탑승 불가");
+            } else {
+                System.out.println("인원 초과, 탑승 불가");
+            }
+        } else if (nowSpeed > 0) {
+            System.out.println("현재 속도 :" + nowSpeed + ", 탑승 불가");
+        }
+    }
+
+    @Override
+    public void changeSpd(int nowSpeed, Status status) {
 
     }
 
     @Override
-    public void changeSpd(int nowSpeed, String status) {
+    public void ChangeStat(int fuel, int nowPassenger, Status status) {
 
     }
 
     @Override
-    public void ChangeStat(int fuel, int nowPassenger, String status) {
-
-    }
-
-    @Override
-    public void start(int transNum, int fuel, int nowSpeed, int maxPassenger, int nowPassenger, String status) {
-
+    public void start(int transNum, int fuel, int nowSpeed, int maxPassenger, int nowPassenger, Status status) {
+        if (fuel > 10) {
+            status = status.운행중;
+        }
     }
 }
--- src/main/java/Transport.java
+++ src/main/java/Transport.java
@@ -10,12 +10,12 @@ public class Transport {
     //- 현재 승객
     int nowPassenger;
     //- 운행 상태
-    String status;
+    Status status;
     // 기본요금
     int fare;
 
 
-    public Transport(int transNum, int fuel, int nowSpeed, int maxPassenger, int nowPassenger, int fare, String status) {
+    public Transport(int transNum, int fuel, int nowSpeed, int maxPassenger, int nowPassenger, int fare, Status status) {
         this.transNum = transNum;
         this.fuel = fuel;
         this.nowSpeed = nowSpeed;
@@ -25,7 +25,7 @@ public class Transport {
         this.status = status;
     }
 
-    public Transport(int transNum, int fuel, int nowSpeed, int nowPassenger, int fare, String status) {
+    public Transport(int transNum, int fuel, int nowSpeed, int nowPassenger, int fare, Status status) {
         this(0, 100, 0, 0,  0, 0, "운행중");
     }
 
